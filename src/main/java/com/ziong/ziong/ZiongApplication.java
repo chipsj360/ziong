d48a1401dd5d38a727d65f98f8bb1d9d904f4260
chipjs360 <josephchipate@gmail.com>
2024-08-19 19:33:16 +0200

    first commit

---
+++ src/main/java/com/ziong/ziong/ZiongApplication.java
@@ -0,0 +1,13 @@
+package com.ziong.ziong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziong/ziong/ZiongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziong.ziong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
