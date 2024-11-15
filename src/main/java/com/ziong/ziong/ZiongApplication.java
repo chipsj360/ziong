8598a0cd1019ff5142022eb72635210cd6c7cd50
WatchMa1 <simbarashekapundu@gmail.com>
2024-11-15 18:18:51 +0200

    more changes on the UI Responsiveness

--- src/main/java/com/ziong/ziong/ZiongApplication.java
+++ src/main/java/com/ziong/ziong/ZiongApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ZiongApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ZiongApplication.class, args);
 	}
 
 }
-
